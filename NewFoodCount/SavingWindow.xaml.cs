9648d50c9261ae0ed4e7e7a4dca4805c14878c0a
Давыдов Денис <den93mos@yandex.ru>
2021-03-21 23:28:04 +0300

    Создание новой формы для сохранения дневного меню и вызов этой формы из окна расчета меню

---
+++ NewFoodCount/SavingWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NewFoodCount
+{
+    /// <summary>
+    /// Логика взаимодействия для SavingWindow.xaml
+    /// </summary>
+    public partial class SavingWindow : Window
+    {
+        public SavingWindow(DishCollection dayDishes)
+        {
+            InitializeComponent();
+            lstResultList.ItemsSource = dayDishes;
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
